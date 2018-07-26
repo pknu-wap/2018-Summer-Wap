53f1dca569549af16a7dda68767b81f4f0f0a246
DESKTOP-TEAPK4H\bbang7 <zxas1245@naver.com>
2018-07-27 01:23:45 +0900

    get urls

--- pythonpro.py
+++ pythonpro.py
@@ -1,17 +1,38 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import requests, re
 
-url = urlopen("http://ce.pknu.ac.kr/05_community/01_community.php")
-soup = BeautifulSoup(url,"html.parser")
-cnt_artist = 0
-cnt_title = 0
+ce = requests.get("http://ce.pknu.ac.kr/05_community/01_community.php").text
+
+soup = BeautifulSoup(ce,"html.parser")
+type = []
+urls = []
+gong = 0
+
+
+for link1 in soup.find_all(name="td", attrs={"class": "first"}):
+    type.append(link1.get_text())
+
+for link1 in soup.find_all(name="td", attrs={"class": "txt-l"}):
+    urls.append(link1.find('a')['href'])
+
+for i in range(len(type)):
+    if type[i] == '공지':
+        gong +=1
+del urls[:gong]
+
+print(urls)
 
 
 
-for link1 in soup.find_all(name="td",attrs={"class":"txt-l"}):
-    cnt_title += 1
-    # print(str(cnt_title))
-    print(link1.text)
+
+
+# for link1 in soup.find_all(name="td",attrs={"class":"first"}):
+#     print(link1.get_text())
+#
+#
+# for link1 in soup.find_all(name="td",attrs={"class":"txt-l"}):
+#     print(link1.find('a')['href'])
 
 
 
