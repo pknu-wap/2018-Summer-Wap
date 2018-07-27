5ee42eb809865ffdc90e5ba71b18db877eb506a5
DESKTOP-TEAPK4H\bbang7 <zxas1245@naver.com>
2018-07-27 17:51:45 +0900

    get url contents

--- pythonpro.py
+++ pythonpro.py
@@ -1,38 +1,37 @@
 from urllib.request import urlopen
-from bs4 import BeautifulSoup
-import requests, re
+from bs4 import BeautifulSoup as bs
+import requests
 
 ce = requests.get("http://ce.pknu.ac.kr/05_community/01_community.php").text
 
-soup = BeautifulSoup(ce,"html.parser")
+ce_table = bs(ce,"html.parser")
 type = []
 urls = []
 gong = 0
 
 
-for link1 in soup.find_all(name="td", attrs={"class": "first"}):
-    type.append(link1.get_text())
+for link in ce_table.find_all(name="td", attrs={"class": "first"}):
+    type.append(link.get_text())
 
-for link1 in soup.find_all(name="td", attrs={"class": "txt-l"}):
-    urls.append(link1.find('a')['href'])
+for link in ce_table.find_all(name="td", attrs={"class": "txt-l"}):
+    urls.append(link.find('a')['href'])
 
 for i in range(len(type)):
     if type[i] == '공지':
         gong +=1
 del urls[:gong]
 
-print(urls)
+
+for url in urls:
+    ce_url = requests.get('http://ce.pknu.ac.kr'+ url).text
+    ce_contents = bs(ce_url,"html.parser")
+
+    for link in ce_contents.find_all(name="tr"):
+        print(link.get_text())
 
 
 
 
-
-# for link1 in soup.find_all(name="td",attrs={"class":"first"}):
-#     print(link1.get_text())
-#
-#
-# for link1 in soup.find_all(name="td",attrs={"class":"txt-l"}):
-#     print(link1.find('a')['href'])
 
 
 
