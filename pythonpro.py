fba0ff9c39b675d6654a19966f6bfe7177d20659
DESKTOP-TEAPK4H\bbang7 <zxas1245@naver.com>
2018-07-28 14:28:33 +0900

    finish collecting contents

--- pythonpro.py
+++ pythonpro.py
@@ -26,9 +26,20 @@ for url in urls:
     ce_url = requests.get('http://ce.pknu.ac.kr'+ url).text
     ce_contents = bs(ce_url,"html.parser")
 
-    for link in ce_contents.find_all(name="tr"):
-        print(link.get_text())
+    for link in ce_contents.find_all(name="tr", attrs={"class":"head"}):
+        titles = link.find_all('td')
+        print('[',titles[2].text,'] ','제목  : ',titles[0].text,'\n')
+    for link in ce_contents.find_all(name="p", attrs={"class": "0"}):
+        print(link.text)
+    print('---------------------------------------------------------------------------------')
 
+
+# ce_url = requests.get('http://ce.pknu.ac.kr'+'/05_community/01_community.php?bid=c_notice&page=0&sv=title&sw=&tgt=view&idx=3657').text
+# ce_contents = bs(ce_url,"html.parser")
+# for link in ce_contents.find_all(name="tr",attrs="head"):
+#     print(link.text)
+# for link in ce_contents.find_all(name="p",attrs = {"class" : "0"}):
+#     print(link.text)
 
 
 
