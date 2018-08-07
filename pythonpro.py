65748284df30d1a50c5708221c2b296e99b8e5e2
DESKTOP-TEAPK4H\bbang7 <zxas1245@naver.com>
2018-08-07 20:44:41 +0900

    insert data without contents

--- pythonpro.py
+++ pythonpro.py
@@ -1,6 +1,14 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as bs
 import requests
+import pymysql
+
+conn = pymysql.connect(host='localhost',
+                       user='root',
+                       password='111111',
+                       db='gohwak',
+                       charset='utf8mb4',
+                       cursorclass=pymysql.cursors.DictCursor)
 
 ce = requests.get("http://ce.pknu.ac.kr/05_community/01_community.php").text
 
@@ -25,21 +33,24 @@ del urls[:gong]
 for url in urls:
     ce_url = requests.get('http://ce.pknu.ac.kr'+ url).text
     ce_contents = bs(ce_url,"html.parser")
+    urrl = ('http://ce.pknu.ac.kr'+ url)
 
     for link in ce_contents.find_all(name="tr", attrs={"class":"head"}):
         titles = link.find_all('td')
-        print('[',titles[2].text,'] ','제목  : ',titles[0].text,'\n')
-    for link in ce_contents.find_all(name="p", attrs={"class": "0"}):
-        print(link.text)
-    print('---------------------------------------------------------------------------------')
+        # print('[',titles[2].text,'] ','제목  : ',titles[0].text,'\n')
+        date = (titles[2].text)
+        title = (titles[0].text)
+    # for link in ce_contents.find_all(name="p", attrs={"class": "0"}):
+        # print(link.text)
 
-
-# ce_url = requests.get('http://ce.pknu.ac.kr'+'/05_community/01_community.php?bid=c_notice&page=0&sv=title&sw=&tgt=view&idx=3657').text
-# ce_contents = bs(ce_url,"html.parser")
-# for link in ce_contents.find_all(name="tr",attrs="head"):
-#     print(link.text)
-# for link in ce_contents.find_all(name="p",attrs = {"class" : "0"}):
-#     print(link.text)
+    try:
+        with conn.cursor() as cursor:
+            sql = 'INSERT INTO cecroll (title, created, url) VALUES ("%s", "%s", "%s")' %(title, date, urrl)
+            cursor.execute(sql)
+        conn.commit()
+    finally:
+        print('FINISH INSERT')
+conn.close()
 
 
 
