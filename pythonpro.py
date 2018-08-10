e9640bc39c8fcdc7667a8bd701f10b3a9c670bcc
DESKTOP-TEAPK4H\bbang7 <zxas1245@naver.com>
2018-08-10 14:47:59 +0900

    insert contents to database

--- pythonpro.py
+++ pythonpro.py
@@ -24,32 +24,38 @@ for link in ce_table.find_all(name="td", attrs={"class": "first"}):
 for link in ce_table.find_all(name="td", attrs={"class": "txt-l"}):
     urls.append(link.find('a')['href'])
 
-for i in range(len(type)):
-    if type[i] == '공지':
-        gong +=1
-del urls[:gong]
+# for i in range(len(type)):
+#     if type[i] == '공지':
+#         gong +=1
+# del urls[:gong]
 
 
 for url in urls:
+    check = 0
     ce_url = requests.get('http://ce.pknu.ac.kr'+ url).text
     ce_contents = bs(ce_url,"html.parser")
     urrl = ('http://ce.pknu.ac.kr'+ url)
-
+    contents = ""
     for link in ce_contents.find_all(name="tr", attrs={"class":"head"}):
         titles = link.find_all('td')
         # print('[',titles[2].text,'] ','제목  : ',titles[0].text,'\n')
         date = (titles[2].text)
         title = (titles[0].text)
-    # for link in ce_contents.find_all(name="p", attrs={"class": "0"}):
-        # print(link.text)
+    for link in ce_contents.find_all(name="p", attrs={"class": "0"}):
+        print(link.text)
+        if check == 0:
+            check = 1
+            contents += link.text
+        else:
+            contents = contents + "\n"+ link.text
 
     try:
         with conn.cursor() as cursor:
-            sql = 'INSERT INTO cecroll (title, created, url) VALUES ("%s", "%s", "%s")' %(title, date, urrl)
+            sql = 'INSERT INTO cecroll (title, created, url, content) VALUES ("%s", "%s", "%s", "%s")' %(title, date, urrl, contents)
             cursor.execute(sql)
         conn.commit()
-    finally:
-        print('FINISH INSERT')
+    except:
+        continue
 conn.close()
 
 
