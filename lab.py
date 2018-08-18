0b104f1a00cf9675f3891f97ad40ba7807f41191
DESKTOP-TEAPK4H\bbang7 <zxas1245@naver.com>
2018-08-18 16:29:22 +0900

    sunsheep dbupdate

--- lab.py
+++ lab.py
@@ -4,9 +4,13 @@ import requests
 import threading
 import time
 ce = requests.get("http://ce.pknu.ac.kr/05_community/01_community.php").text
+cms = requests.get("http://cms.pknu.ac.kr/pkuocean/view.do?no=1253&pageIndex=1&view=list&sv=TITLE&sw=").text
 
 ce_table = bs(ce, "html.parser")
+cms_table = bs(cms,"html.parser")
+
 urls = []
+urls2 = []
 
 class Gohwak:
     def __init__(self):
@@ -31,7 +35,7 @@ class Gohwak:
                 # date = (titles[2].text)
                 # title = (titles[0].text)
             for link in ce_contents.find_all(name="p", attrs={"class": "0"}):
-                print(link.text)
+                # print(link.text)
                 if check == 0:
                     check = 1
                     contents += link.text
@@ -40,16 +44,52 @@ class Gohwak:
                 # print(contents)
             print('--------------------------------------------------------------------------------')
 
-        threading.Timer(10,self.Ce).start()
+        threading.Timer(20,self.Ce).start()
+    def Cms(self):
+        cms = a = cms_table.find(name="ul", attrs={"id": "board_list"})
+        for link in cms.find_all(name="li"):
+            urls2.append(link.find('a')['href'])
+
+        for url in urls2:
+            check = 0
+            cms_url = requests.get('http://cms.pknu.ac.kr' + url).text
+            cms_contents = bs(cms_url, "html.parser")
+
+            urrl = ('http://cms.pknu.ac.kr' + url)
+
+            contents = ""
+            for link in cms_contents.find_all(name="div", attrs={"id": "board_view"}):
+                titlescrap = link.find('h3')
+                namedate = link.find_all('strong')
+
+                title = (titlescrap.text)
+                date = (namedate[0].text)
+                writer = (namedate[1].text)
+
+                print(date,'     ',title,'     ',writer)
+
+            for link in cms_contents.find_all(name="div", attrs={"class": "board_stance"}):
+                # print(link.text)
+                if check == 0:
+                    check = 1
+                    contents += link.text
+                else:
+                    contents = contents + "\n" + link.text
+
+            #     print(contents)
+            print('-----------------------------------------------------------------------------------')
+
+            threading.Timer(10, self.Cms).start()
 
     def Check(self):
         print ('Update fin')
-        threading.Timer(10, self.Check).start()
+        threading.Timer(20, self.Check).start()
 
 def main():
     print ('Go Hwak')
     gh = Gohwak()
     gh.Ce()
+    gh.Cms()
     gh.Check()
 
 if __name__ == '__main__':
--- resunsheep.py
+++ resunsheep.py
@@ -3,32 +3,32 @@ from bs4 import BeautifulSoup as bs
 import requests
 import pymysql
 
-# conn = pymysql.connect(host='localhost',
-#                        user='root',
-#                        password='111111',
-#                        db='gohwak',
-#                        charset='utf8mb4',
-#                        cursorclass=pymysql.cursors.DictCursor)
+conn = pymysql.connect(host='localhost',
+                       user='root',
+                       password='111111',
+                       db='gohwak',
+                       charset='utf8mb4',
+                       cursorclass=pymysql.cursors.DictCursor)
 
-ce = requests.get("http://cms.pknu.ac.kr/pkuocean/view.do?no=1253&pageIndex=1&view=list&sv=TITLE&sw=").text
+cms = requests.get("http://cms.pknu.ac.kr/pkuocean/view.do?no=1253&pageIndex=1&view=list&sv=TITLE&sw=").text
 
-ce_table = bs(ce,"html.parser")
+cms_table = bs(cms,"html.parser")
 urls = []
 
-cms = a = ce_table.find(name="ul", attrs={"id": "board_list"})
+cms = a = cms_table.find(name="ul", attrs={"id": "board_list"})
 for link in cms.find_all(name="li"):
     urls.append(link.find('a')['href'])
 
 
 for url in urls:
     check = 0
-    ce_url = requests.get('http://cms.pknu.ac.kr'+ url).text
-    ce_contents = bs(ce_url,"html.parser")
+    cms_url = requests.get('http://cms.pknu.ac.kr'+ url).text
+    cms_contents = bs(cms_url,"html.parser")
 
     urrl = ('http://cms.pknu.ac.kr'+ url)
 
     contents = ""
-    for link in ce_contents.find_all(name="div", attrs={"id":"board_view"}):
+    for link in cms_contents.find_all(name="div", attrs={"id":"board_view"}):
 
         titlescrap = link.find('h3')
         namedate = link.find_all('strong')
@@ -39,7 +39,7 @@ for url in urls:
 
         # print(date,'     ',title,'     ',writer)
 
-    for link in ce_contents.find_all(name="div", attrs={"class": "board_stance"}):
+    for link in cms_contents.find_all(name="div", attrs={"class": "board_stance"}):
 
         if check == 0:
             check = 1
@@ -49,14 +49,15 @@ for url in urls:
 
     #     print(contents)
     # print('-----------------------------------------------------------------------------------')
-#     # try:
-#     #     with conn.cursor() as cursor:
-#     #         sql = 'INSERT INTO cecroll (title, created, url, content) VALUES ("%s", "%s", "%s", "%s")' %(title, date, urrl, contents)
-#     #         cursor.execute(sql)
-#     #     conn.commit()
-#     # except:
-#     #     continue
-# conn.close()
+
+    try:
+        with conn.cursor() as cursor:
+            sql = 'INSERT INTO cmscroll (title, created, writer, url, content) VALUES ("%s", "%s","%s", "%s", "%s")' %(title, date, writer, urrl, contents)
+            cursor.execute(sql)
+        conn.commit()
+    except:
+        continue
+conn.close()
 
 
 
